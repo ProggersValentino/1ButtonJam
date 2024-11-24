ec586e5864e21bb07bd90464c06b6a9467b57a16
HaileyG123 <n11263997@qut.edu.au>
2024-11-24 17:20:50 +1000

    MusicTrackSO set up

--- Assets/Scripts/Notes/NoteData.cs
+++ Assets/Scripts/Notes/NoteData.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NoteData", menuName = "Scriptable Objects/NoteData")]
 public class NoteData : ScriptableObject
 {
-    
+    public Sprite sprite;
+    public Animation animation;
+    public AudioClip sfx;
 }
